b36f3f1ff9812d8fce2ba8b8383f064f09739a04
Osondu Gabriel <136438699+OsonduGabriel@users.noreply.github.com>
2026-02-14 16:38:00 +0100

    updated js using template literals, destructuring

--- script.js
+++ script.js
@@ -1,57 +1,66 @@
-const containers = document.querySelector(".container")
+const Container = document.querySelector(".container")
 
 async function getData() {
     // fetch data
     try{
         const response = await fetch("https://jsonplaceholder.typicode.com/users")
-        const data = await response.json()
-        const dataSet = data.slice(0, 10)
-        const Users = dataSet.map(user => ({ //create an object for multiple data instead of using a string
-            name: user.name,
-            email: user.email,
-            company: user.company
-        }))
+        const Users = await response.json()
+        // const User = dataSet.map(user => ({ //create an object for multiple data instead of using a string
+        //     name: user.name,
+        //     email: user.email,
+        //     company: user.company
+        // }))
         console.log(Users)
 
         // DOM manipulation
         Users.forEach((user) => {
+            const {name, email, company} = user //destructuring - extracting name,email and company keys from user array that was fetched
+            
             //create the div card
             const Card = document.createElement("div")
-            Card.classList.add("card") //add class "card" to the div
-
-            // name element
-            const name = document.createElement("h2")
-            name.textContent = user.name
-            Card.append(name) //add data to div
-
-            //company name element
-            const company = document.createElement("h4")
-            company.textContent = user.company.name
-            Card.append(company) //add data to div
-
-            // email element
-            const email = document.createElement("p")
-            email.textContent = user.email
-            Card.append(email) //add data to div
-
-
-            //create button
-            const button = document.createElement("button")
-            button.classList.add("btn")
-            button.textContent = "Contact"
-            Card.append(button) //add button div
-            // button action
-            button.addEventListener("click", () => {
-                console.log(user.email)
-                alert(user.email)
+            Card.className = "card"  //add class "card" to the div
+            Card.innerHTML = `
+                    <h2>${name}</h2>
+                    <h4>${company.name}</h4>
+                    <p>${email}</p>
+                    <button class="btn">Contact User</button>
+            `
+           const button = Card.querySelector(".btn")
+           button.addEventListener("click", () => {
+                alert(`You have contacted ${name} at ${email}`)
             })
 
             //add div to main conatiner
-            containers.append(Card) 
+            Container.append(Card) 
         })
     }catch(error){
         console.error("Error when fetching data: ", error)
+        Container.innerHTML = `<p>Failed to load user data. check network connection, check directory...</p>`
     }
 }
 
 getData()
+
+            // DOM MANIPULATION Method 2 - creating elements within the card
+            // name element
+            // const name = document.createElement("h2")
+            // name.textContent = user.name
+            // Card.append(name) //add data to div
+
+            //company name element
+            // const company = document.createElement("h4")
+            // company.textContent = user.company.name
+            // Card.append(company) //add data to div
+
+            // email element
+            // const email = document.createElement("p")
+            // email.textContent = user.email
+            // Card.append(email) //add data to div
+
+
+            //create button
+            // const button = document.createElement("button")
+            // button.classList.add("btn")
+            // button.textContent = "Contact"
+            // Card.append(button) //add button div
+            // button action
